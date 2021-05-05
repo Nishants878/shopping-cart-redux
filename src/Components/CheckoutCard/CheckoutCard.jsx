a4649fc94bd27a79a42b0169ec0e6804e2904af5
Nishants878 <nishants878@gmail.com>
2021-05-05 18:59:19 +0530

    added checkout page

--- src/Components/CheckoutCard/CheckoutCard.jsx
+++ src/Components/CheckoutCard/CheckoutCard.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import classes from './CheckoutCard.module.css'
-export default function CheckoutCard() {
+import classes from './CheckoutCard.module.css';
+import logo from '../../Assest//Icons/airmax.png';
+import plus from '../../Assest/Icons/add.svg';
+import minus from '../../Assest/Icons/minus.svg'
+export default function CheckoutCard(props) {
     return (
         <div className={classes.MainContainer}>
             <div className={classes.ImageWrapper}>
-              
+            <img src={props.img} alt="Shoe"/>
             </div>
             <div className={classes.TextContentWrapper}>
-
+            <h4>Nike Shoe</h4>
+            <div className={classes.IncDecWrapper}>
+               <div className={classes.Decrement}>
+                   <img src={minus} alt="minus"/>
+               </div>
+               <p>01</p>
+               <div className={classes.Increment}>
+               <img src={plus} alt="plus"/>
+               </div>
             </div>
+            <p>Amount: Rs {props.titlePrice}</p>
+            </div>
+            
         </div>
     )
 }
--- src/Container/CheckOut/CheckOut.jsx
+++ src/Container/CheckOut/CheckOut.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import classes from './CheckOut.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import CheckoutCard from '../../Components/CheckoutCard/CheckoutCard'
@@ -8,9 +8,14 @@ import CheckoutCard from '../../Components/CheckoutCard/CheckoutCard'
 export default function CheckOut() {
 
     let  productCount = useSelector((state) => state.productCart);
-    let temp = productCount[0]
-     
+    let totalNumber = productCount.length
     
+    const [cart, setCart] = useState([])
+
+    useEffect(() =>{
+         setCart(productCount)
+    },[productCount])
+
     return (
         <div className={classes.MainContainer}>
         <h1 className={classes.Title}>
@@ -18,8 +23,17 @@ export default function CheckOut() {
         </h1>
         <div className={classes.ProductWrapper}>
           <div className={classes.CardList}>
-              <h3>Total items: 2</h3>
-              <CheckoutCard/>
+              <h3>Total items: {totalNumber}</h3>
+              {
+                 cart.map((item) =>{
+                     return <CheckoutCard id={item.id} key={item.key} titlePrice={item.titlePrice} img={item.img} />
+                 })
+             }
+          </div>
+          <div className={classes.OrderBxWrapper}>
+              <h3>Total Amount</h3>
+              <p>Amount: Rs<span className={classes.Bold}>9598</span></p>
+              <button className={classes.PlaceOrder}>Place Order</button>
           </div>
         </div>
         
